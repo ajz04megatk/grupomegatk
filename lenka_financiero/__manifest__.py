db2c433976697dd803fa1d00585471cf746ea912
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-17 15:48:33 -0600

    [LENKA] Cargar vista de configuracion

--- lenka_financiero/__manifest__.py
+++ lenka_financiero/__manifest__.py
@@ -12,6 +12,7 @@
         'data/sequence.xml',
         'views/lenka_financial_views.xml',
         'views/payment_views.xml',
+        'views/res_config_settings_views.xml',
         'views/lenka_menu.xml',
         'report/lenka_quote_report.xml',
     ],
