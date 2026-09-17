3456cc1be225add6b00ccc93005f571090a7f8dd
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-17 15:58:19 -0600

    [LENKA] Cargar vistas de ciclo de credito

--- lenka_financiero/__manifest__.py
+++ lenka_financiero/__manifest__.py
@@ -13,6 +13,7 @@
         'views/lenka_financial_views.xml',
         'views/payment_views.xml',
         'views/res_config_settings_views.xml',
+        'views/lifecycle_views.xml',
         'views/lenka_menu.xml',
         'report/lenka_quote_report.xml',
     ],
