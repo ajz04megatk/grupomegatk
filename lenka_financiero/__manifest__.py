8b45a86e3de9208107e80b1ea278f35465b2d387
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-17 14:23:26 -0600

    [LENKA] Agregar manifiesto modulo financiero

---
+++ lenka_financiero/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'App Financiero Lenka',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting/Finance',
+    'summary': 'Cotizaciones, prestamos, financiamientos, arrendamientos y fondeo de Inversiones Lenka',
+    'author': 'Grupo MEGATK',
+    'license': 'LGPL-3',
+    'depends': ['base', 'mail', 'account', 'product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'views/lenka_financial_views.xml',
+        'views/lenka_menu.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
