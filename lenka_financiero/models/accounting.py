4ff68a74eb158e13dd2b2df5d183281e01f35e6b
ajz04megatk <89929784+ajz04megatk@users.noreply.github.com>
2026-09-20 16:03:56 -0600

    [LENKA] Registrar comision bancaria de tarjeta separada

--- lenka_financiero/models/accounting.py
+++ lenka_financiero/models/accounting.py
@@ -11,6 +11,7 @@ class ResCompanyLenkaAccounting(models.Model):
     lenka_interest_income_account_id = fields.Many2one('account.account', string='Lenka: Ingreso por intereses')
     lenka_late_fee_income_account_id = fields.Many2one('account.account', string='Lenka: Ingreso por mora')
     lenka_unapplied_account_id = fields.Many2one('account.account', string='Lenka: Cobros no aplicados / anticipos')
+    lenka_card_fee_expense_account_id = fields.Many2one('account.account', string='Lenka: Comision bancaria por tarjeta')
     lenka_funding_cost_expense_account_id = fields.Many2one('account.account', string='Lenka: Gasto por costo de fondeo')
 
 
@@ -23,6 +24,7 @@ class ResConfigSettingsLenkaAccounting(models.TransientModel):
     lenka_interest_income_account_id = fields.Many2one(related='company_id.lenka_interest_income_account_id', readonly=False)
     lenka_late_fee_income_account_id = fields.Many2one(related='company_id.lenka_late_fee_income_account_id', readonly=False)
     lenka_unapplied_account_id = fields.Many2one(related='company_id.lenka_unapplied_account_id', readonly=False)
+    lenka_card_fee_expense_account_id = fields.Many2one(related='company_id.lenka_card_fee_expense_account_id', readonly=False)
     lenka_funding_cost_expense_account_id = fields.Many2one(related='company_id.lenka_funding_cost_expense_account_id', readonly=False)
 
 
@@ -113,6 +115,7 @@ class LenkaPaymentAccounting(models.Model):
         interest_income = company.lenka_interest_income_account_id
         late_income = company.lenka_late_fee_income_account_id
         unapplied_account = company.lenka_unapplied_account_id
+        card_fee_expense = company.lenka_card_fee_expense_account_id
         if not journal or not portfolio or not interest_income or not late_income:
             raise ValidationError(_('Configure el diario de cobros y las cuentas de cartera, intereses y mora de Lenka antes de contabilizar.'))
         if journal.company_id != company:
@@ -127,6 +130,8 @@ class LenkaPaymentAccounting(models.Model):
             raise ValidationError(_('Existe saldo no aplicado. Configure la cuenta de cobros no aplicados / anticipos.'))
         if total_to_book <= 0:
             raise ValidationError(_('El cobro no tiene monto aplicable para contabilizar.'))
+        if self.card_fee_amount and not card_fee_expense:
+            raise ValidationError(_('Configure la cuenta de gasto por comision bancaria de tarjeta antes de contabilizar este cobro.'))
 
         amount_company = self.currency_id._convert(total_to_book, company.currency_id, company, self.payment_date)
         partner = self.partner_id
@@ -139,6 +144,18 @@ class LenkaPaymentAccounting(models.Model):
             'currency_id': self.currency_id.id,
             'amount_currency': total_to_book,
         })]
+
+        if self.card_fee_amount:
+            fee_company = self.currency_id._convert(self.card_fee_amount, company.currency_id, company, self.payment_date)
+            lines.append((0, 0, {
+                'name': _('Comision bancaria tarjeta - %s') % self.name,
+                'partner_id': partner.id,
+                'account_id': card_fee_expense.id,
+                'debit': fee_company,
+                'credit': 0.0,
+                'currency_id': self.currency_id.id,
+                'amount_currency': self.card_fee_amount,
+            }))
 
         def credit_line(account, amount, label):
             if amount <= 0:
@@ -155,6 +172,8 @@ class LenkaPaymentAccounting(models.Model):
             }))
 
         credit_line(portfolio, self.capital_amount, _('Capital cobrado - %s') % self.operation_id.name)
+        if self.card_fee_amount:
+            credit_line(portfolio, self.card_fee_amount, _('Comision de tarjeta no aplicada a deuda - %s') % self.operation_id.name)
         credit_line(interest_income, self.interest_amount, _('Intereses cobrados - %s') % self.operation_id.name)
         credit_line(late_income, self.late_fee_amount, _('Mora cobrada - %s') % self.operation_id.name)
         if self.unapplied_amount:
